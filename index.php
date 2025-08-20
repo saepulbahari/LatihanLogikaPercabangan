730a9cea4e78a90a1d119bec76839af575f570d6
saepulbahari <saepulbahari@gmail.com>
2025-08-20 14:13:44 +0700

    Latihan If Else

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$nilai = 56;
+
+if ($nilai >= 85 && $nilai <= 100 ) {
+    echo "Nilai A";
+} elseif ($nilai >= 75 && $nilai <= 84) {
+    echo "Nilai B";
+} elseif ($nilai >= 60 && $nilai <= 74) {
+    echo "Nilai C";
+} elseif ($nilai >= 50 && $nilai <= 59) {
+    echo "Nilai D";
+} elseif ($nilai >= 0 && $nilai <= 49) {
+    echo "Nilai E";
+} else {
+    echo "Nilai tidak ada";
+
+} 
